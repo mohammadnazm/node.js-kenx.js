49ad1e81ba05fa10797756e76b2fb3e41639e739
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-01-27 20:15:06 +0300

    work on one to many relation ship

--- db/migrations/20230127155626_car-enginner.js
+++ db/migrations/20230127155626_car-enginner.js
@@ -2,10 +2,30 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("Car", function (t) {
+      t.increments("Pk_Car_Id").primary();
+      t.string("Brand");
+      t.string("Model");
+    })
+    .then(function () {
+      return knex.schema.createTable("Engineer", function (t) {
+        t.increments("Pk_Engineer_Id");
+        t.string("FullName");
+        t.string("MobileNo");
+        t.integer("Fk_Car_Id", 11)
+          .unsigned()
+          .references("Pk_Car_Id")
+          .inTable("Car");
+      });
+    });
+};
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTable("Car").dropTable("Engineer");
+};

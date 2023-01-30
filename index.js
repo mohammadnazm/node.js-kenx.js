3535d3e5ecfc2f67e436582f11770039c38159e4
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-01-30 18:53:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(8080, () => console.log("server listening on port 8080"));
+const PORT = process.env.PORT || 8080;
+app.listen(() => console.log(`server listening on port ${PORT}`));
